c15f862724f1074bddd29c46aee23e6fdae01e33
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-10-26 21:45:00 +0200

    Cards created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   </React.StrictMode>
 );
 
